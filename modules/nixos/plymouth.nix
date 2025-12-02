a5715631ba8f094d51066364c49589347efcd073
Michael Finger <michael.finger@icytv.de>
2025-12-02 13:28:57 +0100

    chore(system): generation 80
    <<< /nix/store/a8cyig8hbjw4szhapvjr7ci3ipsybp5v-nixos-system-eagle-26.05.20251130.2d293cb
    >>> /nix/store/cabhhlskd8iq9dpcj7s2507arq0ndnld-nixos-system-eagle-26.05.20251130.2d293cb
    No version or selection state changes.
    Closure size: 1244 -> 1244 (11 paths added, 11 paths removed, delta +0, disk usage +408B).

--- modules/nixos/plymouth.nix
+++ modules/nixos/plymouth.nix
@@ -1,7 +1,15 @@
 {...}: {
   catppuccin.plymouth.enable = true;
 
-  boot.plymouth = {
-    enable = true;
-  };
+  boot.consoleLogLevel = 3;
+  boot.initrd.verbose = true;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "udev.log_priority=3"
+    "rd.systemd.show_status=auto"
+  ];
+  boot.loader.timeout = 0;
+  boot.plymouth.enable = true;
 }
