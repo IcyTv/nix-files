ad4ba9ec57671ef70036689a25dad799b3ebcffe
Michael Finger <michael.finger@icytv.de>
2025-12-02 17:01:45 +0100

    chore(system): generation 98
    <<< /nix/store/04jbp9lkmm6zparbml4f9ihlw6xznxdc-nixos-system-eagle-26.05.20251130.2d293cb
    >>> /nix/store/83bwiz57knaaqybgmkfn2m1pzrglp1xl-nixos-system-eagle-26.05.20251130.2d293cb
    No version or selection state changes.
    Closure size: 1285 -> 1285 (11 paths added, 11 paths removed, delta +0, disk usage +216B).

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -4,6 +4,14 @@
     enable = true;
     systemd.enable = true;
 
+    style = ''
+      * {
+        font-family: "JetBrainsMono Nerd Font";
+        font-size: 17px;
+        min-height: 0;
+      }
+    '';
+
     settings.main = {
       layer = "top";
       position = "top";
