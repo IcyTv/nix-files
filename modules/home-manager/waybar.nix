9ca0a6f5a3852e2d6bf018e947abd2abe5bb1e53
Michael Finger <michael.finger@icytv.de>
2025-12-02 17:33:16 +0100

    feat(system): generation 100
    <<< /nix/store/ib3wp7if304skvz60jcrya21rnlg3nlj-nixos-system-eagle-26.05.20251130.2d293cb
    >>> /nix/store/ikh4mh5ffhsx1dsl49q3896w6gh1b65m-nixos-system-eagle-26.05.20251130.2d293cb
    Added packages:
    [A.]  #1  niri-taskbar  0.3.0
    Closure size: 1285 -> 1286 (13 paths added, 12 paths removed, delta +1, disk usage +7.7MiB).

--- modules/home-manager/waybar.nix
+++ modules/home-manager/waybar.nix
@@ -1,4 +1,30 @@
-{...}: {
+{
+  config,
+  pkgs,
+  ...
+}: let
+  niri-taskbar = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "niri-taskbar";
+    version = "0.3.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "LawnGnome";
+      repo = "niri-taskbar";
+      rev = "v0.3.0+niri.25.08";
+      hash = "sha256-Gbzh4OTkvtP9F/bfDUyA14NH2DMDdr3i6oFoFwinEAg=";
+    };
+
+    cargoHash = "sha256-Ql9iqbbS3DY7o5/PR96c2t4VXKoS1kjZ9k3SfhNdbzE=";
+
+    nativeBuildInputs = [pkgs.pkg-config];
+    buildInputs = [pkgs.gtk3];
+
+    installPhase = ''
+      mkdir -p $out/lib
+      cp target/x86_64-unknown-linux-gnu/release/libniri_taskbar.so $out/lib/
+    '';
+  };
+in {
   catppuccin.waybar.enable = true;
   programs.waybar = {
     enable = true;
@@ -15,6 +41,25 @@
 
       window#waybar {
         background-color: @mantle;
+        transition-property: background-color;
+        transition-duration: 0.5s;
+      }
+
+      window#waybar.hidden {
+        opacity: 0.5;
+      }
+
+      #memory.
+      #custom-power,
+      #wireplumber,
+      #network,
+      #clock,
+      #tray {
+        boder-radius: 4px;
+        margin: 6px 3px;
+        padding: 6px 12px;
+        background-color: @base;
+        color: @mantle;
       }
     '';
 
@@ -35,6 +80,10 @@
         "tray"
         "bluetooth"
       ];
+
+      "cffi/niri-taskbar" = {
+        module_path = "${niri-taskbar}/lib/libniri_taskbar.so";
+      };
     };
   };
 }
