4aba560c9e2a8dda7ac26f702b0015f92b78a391
Michael Finger <michael.finger@icytv.de>
2025-12-02 15:39:20 +0100

    refactor(system): generation 84
    <<< /nix/store/wid25lcqv02lm0qm36zi2jr6xkcnrpfy-nixos-system-eagle-26.05.20251130.2d293cb
    >>> /nix/store/0z0sfj4af50s0jggj1c97jqywr5ybk17-nixos-system-eagle-26.05.20251130.2d293cb
    Removed packages:
    [R.]  #1  bashrc          <none>
    [R.]  #2  fcft            3.3.2
    [R-]  #3  foot            1.25.0, 1.25.0-terminfo
    [R.]  #4  foot.ini        <none>
    [R.]  #5  niri-login.kdl  <none>
    [R.]  #6  zshrc           <none>
    Closure size: 1251 -> 1244 (18 paths added, 25 paths removed, delta -7, disk usage -1,003.6KiB).

--- modules/nixos/greetd.nix
+++ modules/nixos/greetd.nix
@@ -2,64 +2,14 @@
   config,
   pkgs,
   ...
-}: let
-  niriLoginConfig = pkgs.writeText "niri-login.kdl" ''
-    hotkey-overlay {
-      skip-at-startup
-    }
-
-    output "HDMI-A-1" {
-      off
-    }
-    output "DP-3" {
-      off
-    }
-    output "DP-2" {
-      mode "2560x1440@239.970"
-      position x=0 y=0
-      transform "normal"
-    }
-    spawn-at-startup "sh" "-c" "foot -f monospace:size=20 --fullscreen --title tuigreet -e tuigreet --cmd niri-session; ${pkgs.niri}/bin/niri msg action quit"
-
-    window-rule {
-      match app-id="foot"
-      open-fullscreen true
-      border { off; }
-      draw-border-with-background false
-    }
-
-    cursor {
-      hide-when-typing
-      hide-after-inactive-ms 1
-    }
-
-    animations {
-      off
-    }
-
-    layout {
-        gaps 0
-        default-column-width { proportion 1.0; }
-        focus-ring { off; }
-    }
-  '';
-in {
+}: {
   services.greetd = {
     enable = true;
     useTextGreeter = true;
     settings = {
       default_session = {
-        command = "${pkgs.niri}/bin/niri --config ${niriLoginConfig}";
+        command = "${pkgs.tuigreet}/bin/tuigreet --cmd niri --remember --remember-session --remember-user-session --asterisks --time --greeting 'IcyTvs System' --user-menu --theme 'prompt=blue;user=green;user_default=green;password=white;password_default=white;password_hidden=white;borders=blue;text=white;text_secondary=brightblack'";
         user = "greeter";
-      };
-    };
-  };
-
-  programs.foot = {
-    enable = true;
-    settings = {
-      main = {
-        font = "JetBrainsMono Nerd Font";
       };
     };
   };
