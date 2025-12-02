80412f93e480980aa824864633547081dd9595a4
Michael Finger <michael.finger@icytv.de>
2025-12-02 15:43:03 +0100

    chore(system): generation 85
    <<< /nix/store/0z0sfj4af50s0jggj1c97jqywr5ybk17-nixos-system-eagle-26.05.20251130.2d293cb
    >>> /nix/store/ad0rbrckw3g7wzfhmqprjsvqwcsdhx5p-nixos-system-eagle-26.05.20251130.2d293cb
    No version or selection state changes.
    Closure size: 1244 -> 1244 (8 paths added, 8 paths removed, delta +0, disk usage -88B).

--- modules/nixos/greetd.nix
+++ modules/nixos/greetd.nix
@@ -8,7 +8,7 @@
     useTextGreeter = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --cmd niri --remember --remember-session --remember-user-session --asterisks --time --greeting 'IcyTvs System' --user-menu --theme 'prompt=blue;user=green;user_default=green;password=white;password_default=white;password_hidden=white;borders=blue;text=white;text_secondary=brightblack'";
+        command = "${pkgs.tuigreet}/bin/tuigreet --cmd niri --remember --remember-user-session --asterisks --time --user-menu --theme 'prompt=blue;user=green;user_default=green;password=white;password_default=white;password_hidden=white;borders=blue;text=white;text_secondary=brightblack'";
         user = "greeter";
       };
     };
