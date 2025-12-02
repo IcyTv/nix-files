ac40e85e69f6885cea45c63dc6d35f9975bd392c
Michael Finger <michael.finger@icytv.de>
2025-12-02 19:22:57 +0100

    feat(system): generation 116
    <<< /nix/store/8rf1rw1m56cvrc13lgrv8iynasy4d2zj-nixos-system-eagle-26.05.20251130.2d293cb
    >>> /nix/store/kfzqdayvwsfynwdfq5vrzqb8dw8ph7g0-nixos-system-eagle-26.05.20251130.2d293cb
    Added packages:
    [A.]  #1  hm_walkerthemescatppuccinmochaitem.xml   <none>
    [A.]  #2  hm_walkerthemescatppuccinmochastyle.css  <none>
    Closure size: 1326 -> 1328 (12 paths added, 10 paths removed, delta +2, disk usage +2.9KiB).

--- modules/home-manager/walker.nix
+++ modules/home-manager/walker.nix
@@ -3,6 +3,62 @@
     enable = true;
     runAsService = true;
 
-    config.theme = "catppuccin-mocha";
+    config = {
+      theme = "catppuccin-mocha";
+    };
+
+    themes.catppuccin-mocha = {
+      style = ''
+        window {
+            background-color: rgba(30, 30, 45, 0.95);
+            border: 2px solid #cba6f7;
+        }
+
+        box#main {
+            margin: 10px;
+        }
+
+        box#input-box {
+            background-color: #181825;
+            border-radius: 10px;
+            padding: 5px;
+            border: 1px solid #313244;
+        }
+
+        entry {
+            background-color: transparent;
+            color: #cdd6f4;
+            caret-color: #f5e0dc;
+        }
+
+        list {
+            margin-top: 10px;
+        }
+
+        row {
+            border-radius: 10px;
+            padding: 10px;
+        }
+
+        row:selected {
+            background-color: #45475a;
+        }
+      '';
+      layouts = {
+        "item" = ''
+          <item>
+              <box spacing="10">
+           <box>
+               <icon name="icon" size="32"/>
+           </box>
+           <box orientation="vertical">
+               <text name="name" color="#cdd6f4" font="bold"/>
+               <text name="description" color="#a6adc8"/>
+           </box>
+              </box>
+          </item>
+        '';
+      };
+    };
   };
 }
