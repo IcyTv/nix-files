386941b710d2709784d07cd833dcaec45251df6c
Michael Finger <michael.finger@icytv.de>
2025-12-03 01:59:14 +0100

    feat(system): generation 147
    <<< /nix/store/4gv16f1m3aaj88a8f1bgr7xvx5yqsavs-nixos-system-eagle-26.05.20251130.2d293cb
    >>> /nix/store/ij5lcrlcnpm6swbyqk1q3mvsvdrkzgad-nixos-system-eagle-26.05.20251130.2d293cb
    Added packages:
    [A.]  #1  electron                                     38.7.1
    [A.]  #2  electron-unwrapped                           38.7.1
    [A.]  #3  libpng                                       1.6.50
    [A.]  #4  vencord-settings                             <none>
    [A.]  #5  vesktop                                      1.6.1
    [A.]  #6  vesktop-themes-catppuccin-mocha-mauve.theme  <none>
    Closure size: 2014 -> 2020 (17 paths added, 11 paths removed, delta +6, disk usage +303.5MiB).

--- hosts/eagle/home.nix
+++ hosts/eagle/home.nix
@@ -16,6 +16,7 @@
     ../../modules/home-manager/anyrun.nix
     ../../modules/home-manager/swww.nix
     ../../modules/home-manager/neovim.nix
+    ../../modules/home-manager/discord.nix
     # ../../modules/home-manager/walker.nix
     inputs.catppuccin.homeModules.catppuccin
     inputs.nix-index-database.homeModules.nix-index
---
+++ modules/home-manager/discord.nix
@@ -0,0 +1,42 @@
+{...}: {
+  programs.vesktop = {
+    enable = true;
+    vencord.settings = {
+      appBadge = true;
+      arRPC = true;
+      checkUpdates = false;
+      autoUpdate = false;
+      autoUpdateNotification = false;
+      notifyAboutUpdates = false;
+      customTitleBar = false;
+      tray = true;
+      hardwareAcceleration = true;
+      discordBranch = "stable";
+      plugins = {
+        AnonymiseFileNames.enabled = true;
+        BetterFolders.enabled = true;
+        MessageLogger.enabled = true;
+        FakeNitro.enabled = true;
+        BetterSessions.enabled = true;
+        BetterSettings.enabled = true;
+        CallTimer.enabled = true;
+        ClearURLs.enabled = true;
+        CustomRPC.enabled = true;
+        CustomIdle.enabled = true;
+        DisableCallIdle.enabled = true;
+        FavoriteEmojiFirst.enabled = true;
+        FixImagesQuality.enabled = true;
+        FixYoutubeEmbeds.enabled = true;
+        GameActivityToggle.enabled = true;
+        ImageZoom.enabled = true;
+        LastFMRichPresence.enabled = true;
+        ReadAllNotificationsButton.enabled = true;
+        YoutubeAdblock.enabled = true;
+        VolumeBooster.enabled = true;
+        Unindent.enabled = true;
+      };
+    };
+  };
+
+  catppuccin.vesktop.enable = true;
+}
