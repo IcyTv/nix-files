fd052388ead56f8df5f566bba1b6998493530c13
Michael Finger <michael.finger@icytv.de>
2025-12-03 01:35:36 +0100

    feat(system): generation 144
    <<< /nix/store/9amki22nyayvhslqrb2dcd4zm8sfh4wl-nixos-system-eagle-26.05.20251130.2d293cb
    >>> /nix/store/9kn0lb9h48k58ngj1lhr887178vifwjw-nixos-system-eagle-26.05.20251130.2d293cb
    Added packages:
    [A.]  #1  vimplugin-luajit2.1-nui.nvim  0.4.0-1-unstable-0.4.0-1
    [A.]  #2  vimplugin-mini.nvim           2025-11-28
    [A.]  #3  vimplugin-noice.nvim          2025-11-03
    Closure size: 1996 -> 1999 (20 paths added, 17 paths removed, delta +3, disk usage +4.8MiB).

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -8,10 +8,12 @@
     enable = true;
 
     colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
 
+    plugins.lualine.enable = true;
     plugins.treesitter.enable = true;
     plugins.lz-n.enable = true;
     plugins.which-key.enable = true;
+    plugins.noice.enable = true;
+    plugins.mini.enable = true;
   };
 }
