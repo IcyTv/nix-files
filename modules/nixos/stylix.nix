e5f549310f89ab097ce30c2fc334f19d82f8998e
Michael Finger <michael.finger@icytv.de>
2025-12-29 00:38:02 +0100

    chore(system): generation 22
    <<< /nix/store/anbvpkdw5zlxmm6z2zm1h2kbqf65126q-nixos-system-sparrow-25.11.20251226.f560cce
    >>> /nix/store/nhmjvmg9nb0hww7l8wh0q2dxl7pnwhi0-nixos-system-sparrow-25.11.20251226.f560cce
    No version or selection state changes.
    Closure size: 2691 -> 2691 (6 paths added, 6 paths removed, delta +0, disk usage +296B).

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -13,6 +13,18 @@
         package = pkgs.nerd-fonts.jetbrains-mono;
         name = "JetBrainsMono Nerd Font";
       };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
       sizes = {
         terminal = 10;
       };
