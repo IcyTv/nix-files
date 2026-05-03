8a42993d2daa9fbd5bade681fbf5deb0a574c25f
Michael Finger <michael.finger@icytv.de>
2026-05-03 23:34:09 +0200

    chore(system): generation 30
    <<< /nix/store/hlgsj92gwvy4gdinn20yfgbzafq3y7bn-nixos-system-sparrow-25.11.20260413.7e495b7
    >>> /nix/store/x7zivz2qyhf80iivhyfp2bqgd4vqsn1g-nixos-system-sparrow-25.11.20260413.7e495b7
    No version or selection state changes.
    Closure size: 3260 -> 3260 (6 paths added, 6 paths removed, delta +0, disk usage +288B).

--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -8,11 +8,25 @@
   options.my.nixos.core.enable = lib.mkEnableOption "Core NixOS configuration";
 
   config = lib.mkIf config.my.nixos.core.enable {
-    nix.settings.auto-optimise-store = true;
-    nix.settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+    nix.settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org/"
+        "https://icytv.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrURkbJ16ZMNQvspzbCZP16Q="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "icytv.cachix.org-1:epXlDqA5apfoHPIc+Z7Vx6aPN7Tsz2hzik62V5Rs5sQ="
+      ];
+    };
     nix.gc = {
       automatic = true;
       dates = "daily";
