018c753c8cda3e48f1db86ec82b28fdc77c11670
Michael Finger <michael.finger@icytv.de>
2026-05-09 17:54:06 +0200

    chore(system): generation 422
    <<< /nix/store/14v0hkgmgicnx8d3b90ywim583f9vi0a-nixos-system-eagle-25.11.20260413.7e495b7
    >>> /nix/store/dx43nxqimk2f2mbw1vy9cfdh1zw1n1cc-nixos-system-eagle-25.11.20260413.7e495b7
    No version or selection state changes.
    Closure size: 3558 -> 3558 (6 paths added, 6 paths removed, delta +0, disk usage +56B).

--- modules/nixos/geoclue.nix
+++ modules/nixos/geoclue.nix
@@ -7,6 +7,16 @@
 
   config = lib.mkIf config.my.nixos.geoclue.enable {
     location.provider = "geoclue2";
-    services.geoclue2.enable = true;
+    services.geoclue2 = {
+      enable = true;
+      whitelistedAgents = [
+        "de.icytv.Subniri.LocationProvider"
+      ];
+      appConfig."de.icytv.Subniri.LocationProvider" = {
+        isAllowed = true;
+        isSystem = true; # Setting this to true means it doesn't need an agent's approval
+        users = []; # Empty list means allowed for all users
+      };
+    };
   };
 }
