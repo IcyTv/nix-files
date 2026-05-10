65ce6755918ae68aab3a94c9393d7ba33b5771b0
Michael Finger <michael.finger@icytv.de>
2026-05-10 14:07:40 +0200

    chore(system): generation 425
    <<< /nix/store/l9n48q7w8mh0w2p8xcykk94wxyd6dfcp-nixos-system-eagle-25.11.20260413.7e495b7
    >>> /nix/store/qbf7kkwh90f48qybjpayj8ybmvd2mnp4-nixos-system-eagle-25.11.20260413.7e495b7
    No version or selection state changes.
    Closure size: 3555 -> 3555 (10 paths added, 10 paths removed, delta +0, disk usage +248B).

--- modules/home-manager/gammarelay.nix
+++ modules/home-manager/gammarelay.nix
@@ -4,21 +4,21 @@
   lib,
   ...
 }: let
+  version = "0.1.3";
   wl-gammarelay = pkgs.buildGoModule {
     pname = "wl-gammarelay";
-    version = "0.1.3";
+    inherit version;
 
     src = pkgs.fetchFromGitHub {
       owner = "jeremija";
       repo = "wl-gammarelay";
-      rev = "v0.1.3";
+      rev = "v${version}";
       hash = "sha256-XD9BKaNfxH02Fi7szs23HSfLsTbcUlxw30lZTTDOv+E=";
     };
 
     vendorHash = "sha256-yJ6AuL0cmU+rMQNv3lmHQQSVipUZAUFxsLvthIsoS+s=";
 
     buildInputs = with pkgs; [
-      wlr-protocols
       wayland
     ];
 
@@ -27,13 +27,32 @@
       pkg-config
     ];
 
-    preBuild = ''
-      pushd protocol
+    buildPhase = ''
+      runHook preBuild
 
-      make all
+      make protocol build \
+        VERSION=${version} \
+        COMMIT_HASH=nixpkgs \
+        GOFLAGS="-mod=vendor -trimpath"
 
-      popd
+      runHook postBuild
     '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/bin
+
+      make install \
+        PREFIX=$out \
+        VERSION=${version} \
+        COMMIT_HASH=nixpkgs
+
+      runHook postInstall
+    '';
+
+    doCheck = false;
+
     meta = with lib; {
       description = "A client and daemon for changing color temperature and brightness under Wayland via keybindings.";
       homepage = "https://github.com/jeremija/wl-gammarelay";
