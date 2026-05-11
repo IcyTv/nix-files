16b722da329ccb41bb45013b8598165df3e9f3f6
Michael Finger <michael.finger@icytv.de>
2026-05-11 15:23:19 +0200

    chore(system): generation 432
    <<< /nix/store/jyipd8c25f7v23hn4nfqpdvvvmayfi4m-nixos-system-eagle-25.11.20260413.7e495b7
    >>> /nix/store/l4vsnaabcldspb8k8dasb9sm5mcnfk4m-nixos-system-eagle-25.11.20260413.7e495b7
    Version changes:
    [U.]  #1  subniri-config-shell   0f011bd -> 0.1.0
    [U.]  #2  subniri-cxxqt-modules  0f011bd -> 0.1.0
    [U.]  #3  subniri-qml-core       0f011bd -> 0.1.0
    [U.]  #4  subniri-shell          0f011bd -> 0.1.0
    Closure size: 4015 -> 4015 (17 paths added, 17 paths removed, delta +0, disk usage +659.8KiB).

--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -17,14 +17,18 @@
 
       substituters = [
         "https://cache.nixos.org/"
-        "https://nix-community.cachix.org/"
+        "https://nix-community.cachix.org"
         "https://icytv.cachix.org"
+        "https://crane.cachix.org"
+        "https://niri.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrURkbJ16ZMNQvspzbCZP16Q="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "icytv.cachix.org-1:epXlDqA5apfoHPIc+Z7Vx6aPN7Tsz2hzik62V5Rs5sQ="
+        "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
+        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
       ];
 
       trusted-users = ["root" "michael"];
