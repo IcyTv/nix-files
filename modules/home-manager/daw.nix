b9efdda4e2a2333502396d8986b4c1e0a560a4bd
Michael Finger <michael.finger@icytv.de>
2026-06-10 12:45:47 +0200

    chore(system): generation 478
    <<< /nix/store/f2ll45ybm6ycmsspnmpfdwfwadi2wi4p-nixos-system-eagle-26.05.20260528.ec942ba
    >>> /nix/store/f2ll45ybm6ycmsspnmpfdwfwadi2wi4p-nixos-system-eagle-26.05.20260528.ec942ba
    No version or selection state changes.
    Closure size: 3099 -> 3099 (0 paths added, 0 paths removed, delta +0, disk usage +0B).

--- modules/home-manager/daw.nix
+++ modules/home-manager/daw.nix
@@ -13,8 +13,8 @@
     ];
 
     home.file = {
-      ".vst3".source = "${pkgs.helm}/lib/vst3";
-      ".lv2".source = "${pkgs.helm}/lib/lv2";
+      ".vst3/Helm.vst3".source = "${pkgs.helm}/lib/vst3/Helm.vst3";
+      ".lv2/Helm.lv2".source = "${pkgs.helm}/lib/lv2/Helm.lv2";
     };
   };
 }
